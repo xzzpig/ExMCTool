6651151304232744d36b2916f5aebd4b3043a228
xzzpig <1146628972@qq.com>
2015-12-23 01:03:49 +0800

    插件新增从服务器获取聊天记录方法

--- src/com/github/xzzpig/exmctool/listener/ChatListener.java
+++ src/com/github/xzzpig/exmctool/listener/ChatListener.java
@@ -1,45 +1,75 @@
 package com.github.xzzpig.exmctool.listener;
+
 import com.github.xzzpig.exmctool.*;
 import com.github.xzzpig.exmctool.event.*;
-
+import java.io.*;
 import java.text.*;
 import java.util.*;
-
+import org.bukkit.*;
 import org.bukkit.event.*;
 import org.bukkit.event.player.*;
-import org.bukkit.*;
 
 public class ChatListener implements Listener
 {
 	public static List<String> chats = new ArrayList<String>();
-	
+
 	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onChat(PlayerChatEvent event){
 		sendChatMessageToCilent(event.getPlayer().getName(),event.getMessage());
 	}
-	
+
 	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onGetRemoteChatMessage(DataReachEvent event){
 		String[] data = event.getStringData().split(" ");
 		if(data.length!=2||!data[0].equalsIgnoreCase("chat"))
 			return;
-		String sayer = "未知来源<" + event.getCilent().getSocket().getInetAddress().getHostName()+">",
-		message = data[1],
-		broadmessage;
-		if(event.getCilent().getType() == CilentType.Player){
+		String sayer = "未知来源<"+event.getCilent().getSocket().getInetAddress().getHostName()+">",
+			message = data[1],
+			broadmessage;
+		if(event.getCilent().getType()==CilentType.Player){
 			sayer = ((Cilent_Player)event.getCilent()).getName();
-			if(Bukkit.getPlayer(sayer) != null){
+			if(Bukkit.getPlayer(sayer)!=null){
 				Bukkit.getPlayer(sayer).sendMessage(message);
 				sendChatMessageToCilent(sayer,message);
 				return;
 			}
 			sayer = "[远程消息]"+sayer;
 		}
-		broadmessage = sayer +":"+message;
+		broadmessage = sayer+":"+message;
 		Bukkit.broadcastMessage(broadmessage);
 		sendChatMessageToCilent(sayer,message);
+	}
+
+	@EventHandler
+	public void onAskChatMessage(DataReachEvent event){
+		String[] data = event.getStringData().split(" ");
+		if(!data[0].equalsIgnoreCase("getchat"))
+			return;
+		String date = data[1];
+		int amount = -1;
+		Cilent cilent = event.getCilent();
+		try{
+			amount = Integer.valueOf(data[2]);
+		}catch(Exception exp){}
+		File logfile = new File(Bukkit.getPluginManager().getPlugin("ExMCTool").getDataFolder()+"/chat/"+date+".log");
+		List<String> chatlogs = new ArrayList<String>();
+		try{
+			FileInputStream fin = new FileInputStream(logfile);
+			Scanner scanner = new Scanner(fin);
+			while(scanner.hasNextLine()){
+				String message = scanner.nextLine();
+				if(amount == -1)
+					cilent.sendData(message.getBytes());
+				else if(chatlogs.size()>amount){
+					chatlogs.remove(0);chatlogs.add(message);}
+			}
+			if(amount != -1)
+				for(String message:chatlogs)
+					cilent.sendData(message.getBytes());
+		}
+		catch(Exception e){}
 	}
 	
 	private void sendChatMessageToCilent(String sayer,String message){
@@ -54,6 +84,16 @@ public class ChatListener implements Listener
 		for(Cilent cilent:Cilent.cilents){
 			cilent.sendData(message.getBytes());
 		}
-		
+		File dir = new File(Bukkit.getPluginManager().getPlugin("ExMCTool").getDataFolder()+"/chat"),
+			outfile = new File(Bukkit.getPluginManager().getPlugin("ExMCTool").getDataFolder()+"/chat/"+time.split(" ")[0]+".log");
+		dir.mkdirs();
+		try{
+			FileOutputStream fout = new  FileOutputStream(outfile,true);
+			fout.write(("\n"+message).getBytes());
+			fout.close();
+		}
+		catch(Exception e){
+			Bukkit.getLogger().warning("[ExMCTool]聊天记录保存失败");
+		}
 	}
 }
