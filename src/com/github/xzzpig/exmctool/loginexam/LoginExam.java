6909f0556eac12ddd1c2fbd2facd1f9993ba7d0b
John Smith <41772578+huzpsb@users.noreply.github.com>
2020-10-24 23:14:53 +0800

    改用async事件
    async可以防止登录量大时服务器主线程卡顿.

--- src/com/github/xzzpig/exmctool/loginexam/LoginExam.java
+++ src/com/github/xzzpig/exmctool/loginexam/LoginExam.java
@@ -3,39 +3,41 @@ package com.github.xzzpig.exmctool.loginexam;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class LoginExam implements Listener{
 	@EventHandler
-	public void onLogin(PlayerLoginEvent event) throws Exception {
-		String player = event.getPlayer().getName();
+	public void Login(AsyncPlayerPreLoginEvent a) throws Exception {
+		String player = a.getName();
 		if(!LoginPlayer.IsExist(player)){
-			event.disallow(null, "[ExMCTool]ÑéÖ¤Ê§°Ü\nReason:"+LoginError.getError(1));
-			System.out.println("[ExMCTool]"+player+"ÑéÖ¤Ê§°Ü");
+      a.setLoginResult(AsyncPlayerPreLoginEvent.Result.KICK_BANNED);
+      a.setKickMessage("[ExMCTool]éªŒè¯å¤±è´¥\nReason:"+LoginError.getError(1));
+			System.out.println("[ExMCTool]"+player+"éªŒè¯å¤±è´¥");
 			return;
 		}
 		LoginPlayer lp = LoginPlayer.Get(player);
 		lp.SendData("login player");
 		if(!LoginPlayer.Get(player).IsPassed("player")){
-			event.disallow(null, "[ExMCTool]ÑéÖ¤Ê§°Ü\nReason:"+LoginError.getError(2));
-			System.out.println("[ExMCTool]"+player+"ÑéÖ¤Ê§°Ü");
+			a.setLoginResult(AsyncPlayerPreLoginEvent.Result.KICK_BANNED);
+      a.setKickMessage("[ExMCTool]éªŒè¯å¤±è´¥\nReason:"+LoginError.getError(2));
+			System.out.println("[ExMCTool]"+player+"éªŒè¯å¤±è´¥");
 			lp.SendData("login deny");
 			lp.Destory();
 			return;
 		}
 		lp.SendData("login key");
 		if(!LoginPlayer.Get(player).IsPassed("key")){
-			event.disallow(null, "[ExMCTool]ÑéÖ¤Ê§°Ü\nReason:"+LoginError.getError(2));
-			System.out.println("[ExMCTool]"+player+"ÑéÖ¤Ê§°Ü");
+			a.setLoginResult(AsyncPlayerPreLoginEvent.Result.KICK_BANNED);
+      a.setKickMessage("[ExMCTool]éªŒè¯å¤±è´¥\nReason:"+LoginError.getError(2));
+			System.out.println("[ExMCTool]"+player+"éªŒè¯å¤±è´¥");
 			lp.SendData("login deny");
 			lp.Destory();
 			return;
 		}
 		if(LoginPlayer.Get(player).IsPassed()){
-			event.allow();
 			lp.SendData("login pass");
-			System.out.println("[ExMCTool]"+player+"ÑéÖ¤Í¨¹ı");
+			System.out.println("[ExMCTool]"+player+"éªŒè¯é€šè¿‡");
 		}
 	}
 	@EventHandler
